8e8e15bfe028c3bf42278a1d9ed2a3834d3cde32
Egor Lukoyanov <52897993+Egordoesprogramming@users.noreply.github.com>
2021-08-07 20:39:20 -0400

    AsteroidDemo

---
+++ NeuralNetworkAsteroids/Assets/Scripts/GameManager.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+//Egor Lukoyanov (100633412)
+
+//References used: "Neural Networks in Unity" by Abhishek Nandy and Manisha Biswas (Apress, 2018).
+
+
+public class GameManager : MonoBehaviour
+{
+    //public variables
+    public GameObject EnemyPrefab;
+    public GameObject player;
+
+    //training of AI and population generation
+    private bool Traning = false;
+    private int populationSize = 40;
+    private int generationNumber = 0;
+    private int[] layers = new int[] { 1, 10, 10, 1 }; //1 input and 1 output
+
+    //list to contain neural network 
+    private List<NeuralNetwork> nets;
+
+    //list to contain comets
+    private List<Spaceship> spaceshipsList = null;
+
+    void Update()
+    {
+
+        if (Traning == false)
+        {
+            if (generationNumber == 0)
+            {
+                InitCometNeuralNetworks();
+            }
+
+            else
+            {
+                nets.Sort();
+
+                for (int i = 0; i < populationSize / 2; i++)
+                {
+                    nets[i] = new NeuralNetwork(nets[i + (populationSize / 2)]);
+                    nets[i].neuralMutation();
+
+                    nets[i + (populationSize / 2)] = new NeuralNetwork(nets[i + (populationSize / 2)]); //so just going to make a deepcopy lol
+                }
+
+                for (int i = 0; i < populationSize; i++)
+                {
+                    nets[i].setNeuralFitness(0f);
+                }
+            }
+
+            generationNumber++;
+
+            Traning = true;
+            Invoke("Timer", 15f);
+            CreateCometBodies();
+            
+        }
+
+    }
+
+
+    //creating our asteroid AI to follow the spaceship
+    private void CreateCometBodies()
+    {
+        if (spaceshipsList != null)
+        {
+            for (int i = 0; i < spaceshipsList.Count; i++)
+            {
+                GameObject.Destroy(spaceshipsList[i].gameObject);
+            }
+
+        }
+
+        spaceshipsList = new List<Spaceship>();
+
+        //create our asteroids passed on the specified population list
+        for (int i = 0; i < populationSize; i++)
+        {
+            Spaceship spaceship = ((GameObject)Instantiate(EnemyPrefab, new Vector3(UnityEngine.Random.Range(-10f, 10f), UnityEngine.Random.Range(-10f, 10f), 0), EnemyPrefab.transform.rotation)).GetComponent<Spaceship>();
+            spaceship.Init(nets[i], player.transform);
+            spaceshipsList.Add(spaceship);
+        }
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    void InitCometNeuralNetworks()
+    {
+        //population must be even, just setting it to 20 incase it's not
+        if (populationSize % 2 != 0)
+        {
+            populationSize = 20;
+        }
+
+        //instantiate our neural network 
+        nets = new List<NeuralNetwork>();
+
+        //create a new list for mutations to be applied to, the add this to our original neural network list
+        for (int i = 0; i < populationSize; i++)
+        {
+            NeuralNetwork net = new NeuralNetwork(layers);
+            net.neuralMutation();
+            nets.Add(net);
+        }
+    }
+
+ 
+    void Timer()
+    {
+        Traning = false;
+    }
+
+}
---
+++ NeuralNetworkAsteroids/Assets/Scripts/Spaceship.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+//Alvin Aniwa (100603501)
+//Cameron van Velzen (100591663)
+
+//References used: "Neural Networks in Unity" by Abhishek Nandy and Manisha Biswas (Apress, 2018).
+
+public class Spaceship : MonoBehaviour {
+    //has our game started
+    private bool initilized = false;
+    private Transform player;
+
+    private NeuralNetwork net;
+    private Rigidbody2D rBody;
+
+    public int colCounter = 0;
+    //UI to display collisions
+    public Text trackCollision;
+
+    [SerializeField]
+    Transform rotationCenter;
+
+    [SerializeField]
+    float rotationRadius = 30f, angularSpeed = 0.5f;
+
+    float posX, posY, angle = 0f;
+
+    void Start()
+    {
+
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        posX = rotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
+        posY = rotationCenter.position.y + Mathf.Sin(angle) * rotationRadius;
+        transform.position = new Vector2(posX, posY);
+        angle = angle + Time.deltaTime * angularSpeed;
+
+        trackCollision.text = "Total Number of Collsions: " + colCounter;
+    }
+
+    void FixedUpdate ()
+    {
+        if (initilized == true)
+        {
+            float distance = Vector2.Distance(transform.position, player.position);
+            if (distance > 20f)
+                distance = 20f;
+
+            float[] inputs = new float[1];
+
+
+            float angle = transform.eulerAngles.z % 360f;
+            if (angle < 0f)
+                angle += 360f;
+
+            Vector2 deltaVector = (player.position - transform.position).normalized;
+   
+
+            float rad = Mathf.Atan2(deltaVector.y, deltaVector.x);
+            rad *= Mathf.Rad2Deg;
+
+            rad = rad % 360;
+            if (rad < 0)
+            {
+                rad = 360 + rad;
+            }
+
+            rad = 90f - rad;
+            if (rad < 0f)
+            {
+                rad += 360f;
+            }
+            rad = 360 - rad;
+            rad -= angle;
+            
+            if (rad < 0)
+                rad = 360 + rad;
+            
+            if (rad >= 180f)
+            {
+                rad = 360 - rad;
+                rad *= -1f;
+            }
+            rad *= Mathf.Deg2Rad;
+			
+            inputs[0] = rad / (Mathf.PI);
+
+
+            float[] output = net.FeedMeForward(inputs);
+
+            rBody.velocity = 5.5f * transform.up;
+            rBody.angularVelocity = 500f * output[0];
+
+            net.addNeuralFitness((1f-Mathf.Abs(inputs[0])));
+        }
+	}
+
+    public void Init(NeuralNetwork net, Transform pl)
+    {
+        this.player = pl;
+        this.net = net;
+        initilized = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Enemy2(Clone)")
+        {
+            colCounter++;
+        }
+    }
+}
